e0d350f91676a959d3d663e4a059a4c21a7876f5
Artem Arbatskiy <arbatskiy.ings@gmail.com>
2016-11-08 10:36:39 +0000

    Finish ideal weight exersize

---
+++ 1_Ruby_Basics/ideal_weight.rb
@@ -0,0 +1,16 @@
+puts "Hello, what's your name?"
+user_name = gets.chomp
+
+puts "Nice to meet you, #{user_name}. What is your height?"
+user_height = gets.chomp.to_i
+
+ideal_weight = user_height - 110
+
+if ideal_weight > 0
+    puts "#{user_name}, your ideal_weight is #{ideal_weight}!"
+else 
+    puts "Cheers! Your weight is ideal already!"
+end
+
+
+
