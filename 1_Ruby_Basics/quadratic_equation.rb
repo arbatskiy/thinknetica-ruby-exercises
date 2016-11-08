0014aa47fba3fb11038895a894817e3eda252e47
Artem Arbatskiy <arbatskiy.ings@gmail.com>
2016-11-08 11:45:36 +0000

    Solve quadratic_equation quiz

---
+++ 1_Ruby_Basics/quadratic_equation.rb
@@ -0,0 +1,23 @@
+puts "Input A"
+a = gets.chomp.to_f
+puts "Input B"
+b = gets.chomp.to_f
+puts "Input C"
+c = gets.chomp.to_f
+
+d = b * b - 4 * a * c
+x1 = ""
+x2 = ""
+
+if d > 0
+    x1 = (-b + Math.sqrt(d)) / 2 * a
+    x2 = (-b - Math.sqrt(d)) / 2 * a
+    puts "D = #{d}, x1 = #{x1}, x2 = #{x2}"
+elsif d == 0
+    x1 = -b / 2 * a
+    puts "D = #{d}, x1 = x2 = #{x1}"
+else
+    puts "The equation can't be solved"
+end
+
+    
