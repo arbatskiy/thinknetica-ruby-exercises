2fd695b2c7e659e21aedaebc4f39eecd9668b3a0
Artem Arbatskiy <arbatskiy.ings@gmail.com>
2016-11-08 10:48:17 +0000

    Change triangle area to floats

--- 1_Ruby_Basics/triangle_area.rb
+++ 1_Ruby_Basics/triangle_area.rb
@@ -1,7 +1,7 @@
 puts "What is the triangle base?"
-base = gets.chomp.to_i
+base = gets.chomp.to_f
 puts "What is the height?"
-height = gets.chomp.to_i
+height = gets.chomp.to_f
 
 area = 1.0 / 2 * base * height
 puts "Area is #{area}"
